2e6f0fffd9e83435a0e0f7fb5b18f9bc17e0f953
Anshuman Suri <anshuman14021@iiitd.ac.in>
2017-03-22 23:25:56 +0530

    Data generation script

--- README.md
+++ README.md
@@ -15,3 +15,36 @@ Run the folliwng commands (in order):
 ### Running it
 
 * `python populate_tables.py <dbname>` to create a database 'dbname' and create tables appropriately.
+
+
+### Description
+
+
+Actor:
+	a_id [1,200000]
+	name [15string]	
+
+Production Company:
+	pc_id [1,50000]
+	name [10string]
+	address [30string]
+
+Movie:
+	m_id [1,1000000]
+	name [10string]
+	year [1900,2000]
+	imdb score [1.0,5.0] (95% of scores in [1.0,2.0], 5% of scores in (2.0,5.0]
+	pc_id ( 90% are in [1,100], 10% are in [11,50000])
+
+Casting:
+	m_id
+	a_id
+
+Each movie has 4 actors, all movies have actors
+
+Indices on:
+	actor table name
+	name in movie table
+	year in movie
+	m_id in Casting
+	a_id in casting
--- push_data.py
+++ push_data.py
@@ -1,36 +1,73 @@
-import psycopg2
-from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT
-import sys
-
+# import psycopg2
+# from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT
 import numpy as np
-
 # 2014089, 2014021
+import random
+from random import choice
+from string import ascii_lowercase
 
-# Actor:
-# 	a_id [1,200000]
-# 	name [15string]	
 
-# Production Company:
-# 	pc_id [1,50000]
-# 	name [10string]
-# 	address [30string]
+def random_string(size):
+	return ''.join(choice(ascii_lowercase) for i in range(size))
 
-# Movie:
-# 	m_id [1,1000000]
-# 	name [10string]
-# 	year [1900,2000]
-# 	imdb score [1.0,5.0] (95% of scores in [1.0,2.0], 5% of scores in (2.0,5.0]
-# 	pc_id ( 90% are in [1,100], 10% are in [11,50000])
 
-# Casting:
-# 	m_id
-# 	a_id
+def get_actors(size):
+	a_id = range(1,size+1)
+	names = []
+	for i in range(size):
+		names.append(random_string(15))
+	return a_id, names
 
-# Each movie has 4 actors, all movies have actors
 
-# Indices on:
-# 	actor table name
-# 	name in movie table
-# 	year in movie
-# 	m_id in Casting
-# 	a_id in casting
+def get_companies(size):
+	pc_ids = range(1, size+1)
+	names = []
+	addresses = []
+	for i in range(size):
+		names.append(random_string(10))
+		addresses.append(random_string(30))
+	return pc_ids, names, addresses
+
+
+def get_movies(size):
+	m_ids = range(1, size+1)
+	names = []
+	years = []
+	imdb_scores = []
+	pc_ids = []
+	for i in range(size):
+		names.append(random_string(10))
+		years.append(random.randint(1900,2000))
+	for i in range(int(0.95*size)):
+		imdb_scores.append(np.random.rand() + 1.0)
+	for i in range(int(size - (0.95*size))):
+		imdb_scores.append(np.random.rand()*3.0 + 2.0)
+	random.shuffle(imdb_scores) 
+	for i in range(int(0.90*size)):
+		pc_ids.append(np.random.randint(1,10))
+	for i in range(int(size - (0.90*size))):
+		pc_ids.append(np.random.randint(11,50000))
+	random.shuffle(pc_ids)
+	return m_ids, names, years, imdb_scores, pc_ids
+
+
+def get_castings(size, sample):
+	m_ids = []
+	a_ids = []
+	for i in range(1,size+1):
+		actors = np.random.choice(sample, 4)
+		for actor in actors:
+			m_ids.append(i)
+			a_ids.append(actor)
+	return m_ids, a_ids
+
+
+if __name__ == "__main__":
+	get_actors(200000)
+	print "Genarated GenaratedActors database"
+	get_companies(50000)
+	print "Genarated Companies database"
+	get_movies(1000000)
+	print "Genarated Movies database"
+	get_castings(1000000, 200000)
+	print "Genarated Castings database"
